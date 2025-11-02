3423e7468a29230ef7bce3fc05ce1e89233566e1
udithindrachapa <udithindrachapa26@gmail.com>
2025-11-02 15:17:42 +0530

    initial commit

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php
 include 'includes/header.php';
 ?>
-<h2>Welcome</h2>
+<div class="section">
+    <div class="text_sec">
+        <h1>Rent<br>the<br>Perfect<br>Suit<br>for<br>Every<br>Occasion</h1>
+    </div>
+</div>
+
+<div class="brands">
+    <img src="assets/images/brands/Brioni_logo_PNG_(1).png" alt="Brioni">
+    <img src="assets/images/brands/NB-Logo-Black_80-1.png" alt="nb">
+    <img src="assets/images/brands/Giorgio_Armani_(2).png" alt="Giorgio">
+    <img src="assets/images/brands/output-onlinepngtools.png" alt="Prada">
+    <img src="assets/images/brands/Ermenegildo_Zegna_(1).png" alt="Versace">
+</div>
+
+<div class="main_content">
+    <p>Look your best without spending a fortune — rent top-quality suits<br>from trusted brands in Sri Lanka.</p>
+    
+</div>
+
+<?php include 'includes/footer.php'; ?>

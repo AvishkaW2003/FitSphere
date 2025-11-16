2db625b77fca5f72ff97873b6066b1f21b556dcb
udithindrachapa <udithindrachapa26@gmail.com>
2025-11-16 09:55:47 +0530

    Updated Collection, RentNow, Cart pages.

--- collection.php
+++ collection.php
@@ -1,3 +1,7 @@
+<?php
+include 'includes/header.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,236 +11,199 @@
     <link rel="stylesheet" href="assets/css/collection.css">
 </head>
 <body>
-    <?php include 'includes/header.php'; ?>
-    <div class="collection" id="Business_suit">
-        <h2>Business Suits</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/b01.webp" alt="Business Suits 01">
-                <h3>Single-Breasted Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="rentNow.php">Rent Now</a>
-            </div>
 
-            <div class="col">
-                <img src="assets/images/suits/b02.jpg" alt="Business Suits 01">
-                <h3>Double-Breasted Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="rentNow.php">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/b03.jpg" alt="Business Suits 01">
-                <h3>Three-Piece Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="rentNow.php">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/b04.jpg" alt="Business Suits 01">
-                <h3>Slim-Fit Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="rentNow.php">Rent Now</a>
-            </div>
+<!-- Business Suits -->
+<div class="collection" id="Business_suit">
+    <h2>Business Suits</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/b01.webp" alt="Single-Breasted Suit">
+            <h3>Single-Breasted Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=101&name=Single-Breasted Suit&price=2500&image=b01.webp">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/b02.jpg" alt="Double-Breasted Suit">
+            <h3>Double-Breasted Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=102&name=Double-Breasted Suit&price=2500&image=b02.jpg">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/b03.jpg" alt="Three-Piece Suit">
+            <h3>Three-Piece Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=103&name=Three-Piece Suit&price=2500&image=b03.jpg">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/b04.jpg" alt="Slim-Fit Suit">
+            <h3>Slim-Fit Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=104&name=Slim-Fit Suit&price=2500&image=b04.jpg">Rent Now</a>
         </div>
     </div>
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <div class="collection" id="Dinner_suit">
-        <h2>Dinner Suits</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/d01.webp" alt="Business Suits 01">
-                <h3>Classic Black Tuxedo</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/d02.webp" alt="Business Suits 01">
-                <h3>White Dinner Jacket</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/d3.webp" alt="Business Suits 01">
-                <h3>Midnight Blue Tuxedo</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/d04.webp" alt="Business Suits 01">
-                <h3>Velvet Dinner Jacket</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
+<!-- Dinner Suits -->
+<div class="collection" id="Dinner_suit">
+    <h2>Dinner Suits</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/d01.webp" alt="Classic Black Tuxedo">
+            <h3>Classic Black Tuxedo</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=201&name=Classic Black Tuxedo&price=2500&image=d01.webp">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/d02.webp" alt="White Dinner Jacket">
+            <h3>White Dinner Jacket</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=202&name=White Dinner Jacket&price=2500&image=d02.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/d3.webp" alt="Midnight Blue Tuxedo">
+            <h3>Midnight Blue Tuxedo</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=203&name=Midnight Blue Tuxedo&price=2500&image=d3.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/d04.webp" alt="Velvet Dinner Jacket">
+            <h3>Velvet Dinner Jacket</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=204&name=Velvet Dinner Jacket&price=2500&image=d04.webp">Rent Now</a>
         </div>
     </div>
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <div class="collection" id="Wedding_Suits">
-        <h2>Wedding Suits</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/w01.jpg" alt="Business Suits 01">
-                <h3>Classic Black Wedding Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/w02.webp" alt="Business Suits 01">
-                <h3>Navy Blue Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/w03.webp" alt="Business Suits 01">
-                <h3>Light Grey or Beige Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/w04.jpg" alt="Business Suits 01">
-                <h3>Three-Piece Suit (with Vest)</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
+<!-- Wedding Suits -->
+<div class="collection" id="Wedding_Suits">
+    <h2>Wedding Suits</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/w01.jpg" alt="Classic Black Wedding Suit">
+            <h3>Classic Black Wedding Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=301&name=Classic Black Wedding Suit&price=2500&image=w01.jpg">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/w02.webp" alt="Navy Blue Suit">
+            <h3>Navy Blue Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=302&name=Navy Blue Suit&price=2500&image=w02.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/w03.webp" alt="Light Grey or Beige Suit">
+            <h3>Light Grey or Beige Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=303&name=Light Grey or Beige Suit&price=2500&image=w03.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/w04.jpg" alt="Three-Piece Suit with Vest">
+            <h3>Three-Piece Suit (with Vest)</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=304&name=Three-Piece Suit with Vest&price=2500&image=w04.jpg">Rent Now</a>
         </div>
     </div>
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <div class="collection" id="Nilame_Suits">
-        <h2>Nilame Suits</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/n01.jpg" alt="Business Suits 01">
-                <h3>Traditional Kandyan Nilame Attire</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/n02.jpg" alt="Business Suits 01">
-                <h3>Royal Gold Nilame Outfit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/n03.webp" alt="Business Suits 01">
-                <h3>White and Silver Nilame Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/n04.jpg" alt="Business Suits 01">
-                <h3>Regal Black Nilame Suit</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
+<!-- Nilame Suits -->
+<div class="collection" id="Nilame_Suits">
+    <h2>Nilame Suits</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/n01.jpg" alt="Traditional Kandyan Nilame Attire">
+            <h3>Traditional Kandyan Nilame Attire</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=401&name=Traditional Kandyan Nilame Attire&price=2500&image=n01.jpg">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/n02.jpg" alt="Royal Gold Nilame Outfit">
+            <h3>Royal Gold Nilame Outfit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=402&name=Royal Gold Nilame Outfit&price=2500&image=n02.jpg">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/n03.webp" alt="White and Silver Nilame Suit">
+            <h3>White and Silver Nilame Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=403&name=White and Silver Nilame Suit&price=2500&image=n03.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/n04.jpg" alt="Regal Black Nilame Suit">
+            <h3>Regal Black Nilame Suit</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=404&name=Regal Black Nilame Suit&price=2500&image=n04.jpg">Rent Now</a>
         </div>
     </div>
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <div class="collection" id="Indian_Suits">
-        <h2>Indian Suits</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/i01.webp" alt="Business Suits 01">
-                <h3>Sherwani</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/i2.webp" alt="Business Suits 01">
-                <h3>Kurta Pajama</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/i03.webp" alt="Business Suits 01">
-                <h3>Bandhgala (Jodhpuri Suit)</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/i04.webp" alt="Business Suits 01">
-                <h3>Achkan</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
+<!-- Indian Suits -->
+<div class="collection" id="Indian_Suits">
+    <h2>Indian Suits</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/i01.webp" alt="Sherwani">
+            <h3>Sherwani</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=501&name=Sherwani&price=2500&image=i01.webp">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/i2.webp" alt="Kurta Pajama">
+            <h3>Kurta Pajama</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=502&name=Kurta Pajama&price=2500&image=i2.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/i03.webp" alt="Bandhgala (Jodhpuri Suit)">
+            <h3>Bandhgala (Jodhpuri Suit)</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=503&name=Bandhgala (Jodhpuri Suit)&price=2500&image=i03.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/i04.webp" alt="Achkan">
+            <h3>Achkan</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=504&name=Achkan&price=2500&image=i04.webp">Rent Now</a>
         </div>
     </div>
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <div class="collection" id="Blazers">
-        <h2>Blazers</h2>
-        
-        <div class="collection01">
-            <div class="col">
-                <img src="assets/images/suits/b1.webp" alt="Business Suits 01">
-                <h3>Single-Breasted Blazer</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/b2.webp" alt="Business Suits 01">
-                <h3>Double-Breasted Blazer</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/b3.webp" alt="Business Suits 01">
-                <h3>Sports Blazer (Casual Blazer)</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
-
-            <div class="col">
-                <img src="assets/images/suits/b4.webp" alt="Business Suits 01">
-                <h3>Velvet or Tuxedo Blazer</h3>
-                <p>Rs2500.00</p>
-                <a href="#">Rent Now</a>
-            </div>
+<!-- Blazers -->
+<div class="collection" id="Blazers">
+    <h2>Blazers</h2>
+    <div class="collection01">
+        <div class="col">
+            <img src="assets/images/suits/b1.webp" alt="Single-Breasted Blazer">
+            <h3>Single-Breasted Blazer</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=601&name=Single-Breasted Blazer&price=2500&image=b1.webp">Rent Now</a>
         </div>
-
-        <div class="explore">
-            <a href="#">See more ➜ </a>
+        <div class="col">
+            <img src="assets/images/suits/b2.webp" alt="Double-Breasted Blazer">
+            <h3>Double-Breasted Blazer</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=602&name=Double-Breasted Blazer&price=2500&image=b2.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/b3.webp" alt="Sports Blazer (Casual Blazer)">
+            <h3>Sports Blazer (Casual Blazer)</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=603&name=Sports Blazer&price=2500&image=b3.webp">Rent Now</a>
+        </div>
+        <div class="col">
+            <img src="assets/images/suits/b4.webp" alt="Velvet or Tuxedo Blazer">
+            <h3>Velvet or Tuxedo Blazer</h3>
+            <p>Rs2500.00</p>
+            <a href="rentNow.php?id=604&name=Velvet Tuxedo Blazer&price=2500&image=b4.webp">Rent Now</a>
         </div>
     </div>
-    
+    <div class="explore"><a href="#">See more ➜</a></div>
+</div>
 
-    <?php include 'includes/footer.php'; ?>
+<?php include 'includes/footer.php'; ?>
 </body>
 </html>
--- rentNow.php
+++ rentNow.php
@@ -1,5 +1,15 @@
 <?php
 include 'includes/header.php';
+
+// Get product data from GET
+$id = $_GET['id'] ?? null;
+$image = $_GET['image'] ?? 'default.jpg';
+$name = $_GET['name'] ?? 'Product Name';
+$price = $_GET['price'] ?? 0;
+
+if (!$id || !$name) {
+    die("Invalid product selected.");
+}
 ?>
 
 <!DOCTYPE html>
@@ -9,38 +19,38 @@ include 'includes/header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rent | FitSphere</title>
     <link rel="stylesheet" href="assets/css/RentNow.css">
+
 </head>
 <body>
-
-
+    
 <div class="rent-page">
     <div class="rent-container">
         
         <div class="rent-image">
-            <img src="assets/images/suits/b01.webp" alt="Classic Charcoal Executive Suit">
+            <img src="assets/images/suits/<?php echo htmlspecialchars($image); ?>" alt="<?php echo htmlspecialchars($name); ?>">
         </div>
 
         <div class="rent-details">
-            <h2>Classic Charcoal Executive Suit</h2>
-            <p class="description">
-                Step into sophistication with this timeless charcoal three-piece suit. 
-                Tailored for comfort and elegance, perfect for corporate events, weddings, and formal gatherings.
-            </p>
+            <h2><?php echo htmlspecialchars($name); ?></h2>
+
+            <p class="price">Rs. <?php echo number_format($price); ?></p>
+
+            <button class="measurement-btn" onclick="openMeasurement()">Size Guide</button>
 
             <form action="add_to_cart.php" method="GET">
-                <input type="hidden" name="id" value="101">
-                <input type="hidden" name="name" value="Classic Charcoal Executive Suit">
-                <input type="hidden" name="price" value="4500">
-                <input type="hidden" name="image" value="b01.webp">
+                <input type="hidden" name="id" value="<?php echo $id; ?>">
+                <input type="hidden" name="name" value="<?php echo htmlspecialchars($name); ?>">
+                <input type="hidden" name="price" value="<?php echo $price; ?>">
+                <input type="hidden" name="image" value="<?php echo htmlspecialchars($image); ?>">
 
                 <div class="option">
                     <label>Quantity:</label>
-                    <input type="number" name="qty" value="1" min="1">
+                    <input type="number" name="qty" value="1" min="1" required>
                 </div>
 
                 <div class="option">
                     <label>Size:</label>
-                    <select name="size">
+                    <select name="size" required>
                         <option>S</option>
                         <option>M</option>
                         <option>L</option>
@@ -60,12 +70,80 @@ include 'includes/header.php';
                 </div>
 
                 <button type="submit" class="add-cart">Add to Cart</button>
+                <a href="collection.php" class="cancel">Cancel</a>
             </form>
 
-            <a href="collection.php" class="cancel">Cancel</a>
+            
         </div>
     </div>
+
+    
+    <!-- ------------------ Reviews Section ------------------- -->
+    <div class="reviews-box">
+        <h3>Customer Reviews</h3>
+
+        <div class="review">
+            <p style="color:#777; font-style:italic;">No reviews yet for this product.</p>
+        </div>
+    </div>
+
 </div>
+
+<!-- ------------------ Measurement Modal ------------------- -->
+<div class="measurement-modal" id="measureModal">
+    <div class="measurement-content">
+        <span class="close-modal" onclick="closeMeasurement()">&times;</span>
+        <h3>Measurement Guide</h3>
+
+        <table class="measure-table">
+            <tr>
+                <th>Size</th>
+                <th>Chest</th>
+                <th>Waist</th>
+                <th>Height</th>
+            </tr>
+            <tr>
+                <td>S</td>
+                <td>34–36"</td>
+                <td>28–30"</td>
+                <td>5'4 – 5'6"</td>
+            </tr>
+            <tr>
+                <td>M</td>
+                <td>37–39"</td>
+                <td>31–33"</td>
+                <td>5'6 – 5'8"</td>
+            </tr>
+            <tr>
+                <td>L</td>
+                <td>40–42"</td>
+                <td>34–36"</td>
+                <td>5'8 – 6'0"</td>
+            </tr>
+            <tr>
+                <td>XL</td>
+                <td>43–45"</td>
+                <td>37–39"</td>
+                <td>6'0 – 6'2"</td>
+            </tr>
+            <tr>
+                <td>XXL</td>
+                <td>46–48"</td>
+                <td>40–42"</td>
+                <td>6'2 – 6'4"</td>
+            </tr>
+        </table>
+    </div>
+</div>
+
+<script>
+function openMeasurement() {
+    document.getElementById("measureModal").style.display = "flex";
+}
+function closeMeasurement() {
+    document.getElementById("measureModal").style.display = "none";
+}
+</script>
 
 <?php include 'includes/footer.php'; ?>
 </body>
